01b5c47b4e3dcf3aeda257d3b634eda9c5481448
Heinrich Strauss <git@hstrauss.co.za>
2015-08-25 22:10:22 +0200

    (2015.08.25-22h10) updates: {}

---
+++ data/index_content_script.js
@@ -0,0 +1,6 @@
+console.log('panel created.');
+window.onload = function () {
+        console.log('window completely loaded.');
+    }
+    // self.port.on('fxhs-ready', function (data) {});
+
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+"use strict";
 var self = require('sdk/self');
+// var worker = Worker('./lib/info_lookup.js');
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
+/*
 function dummy(text, callback) {
     callback(text);
 }
 exports.dummy = dummy;
+*/
 var {
     ToggleButton
 } = require('sdk/ui/button/toggle');
 var buttons = require('sdk/ui/button/action');
 var panels = require('sdk/panel');
 var tabs = require('sdk/tabs');
+var info_lookup = require('./lib/info_lookup');
 var button = ToggleButton({
     id: 'hstools-b',
     label: 'Show Network Info',
@@ -22,12 +27,16 @@ var button = ToggleButton({
     onClick: button_click_handler
 });
 var panel = panels.Panel({
-    width: 200,
-    height: 200,
+    width: 300,
+    height: 400,
+    onHide: panel_show_handler,
     onHide: panel_hide_handler,
-    contentURL: './panel.html',
-    contentScriptFile: './index_content_script.js'
+    contentURL: './panel.html'
+        // ,contentScriptFile: './index_content_script.js'
 });
+tabs.on('activate', tabs_activate_handler);
+tabs.on('ready', tabs_ready_handler);
+tabs.on('load', tabs_load_handler);
 
 function button_click_handler(state) {
     if (state.checked) {
@@ -38,9 +47,33 @@ function button_click_handler(state) {
         panel.hide();
     }
 }
-// panel.show();
+
 function panel_hide_handler() {
-    button.state('window', {
+    button.state('tab', {
         checked: false
     });
 }
+
+function panel_show_handler() {
+    // button.state('tab', {
+    // checked: false
+    // });
+    console.log('panel_show_handler()');
+}
+
+function tabs_activate_handler(tab) {
+    console.log("tabs_activate_handler(): " + tab.url);
+}
+
+function tabs_ready_handler(tab) {
+    console.log("tabs_ready_handler(): " + tab.url);
+    panel.port.on('respond_info', function (data) {
+        console.log('Received emitted message: ' + data);
+    });
+    info_lookup.get_network_info_from_url(tab.url);
+}
+
+function tabs_load_handler(tab) {
+    console.log("tabs_load_handler(): " + tab.url);
+}
+
