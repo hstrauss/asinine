bd99103b44ff5f653a1d936458127f6bcf5f0762
Heinrich Strauss <git@hstrauss.co.za>
2015-08-28 11:48:56 +0200

    (2015.08.28-11h48) updates: {}

--- data/index_content_script.js
+++ data/index_content_script.js
@@ -3,4 +3,9 @@ window.onload = function () {
         // console.log('window completely loaded.');
     }
     // self.port.on('fxhs-ready', function (data) {});
+self.port.on('', updatePanel);
+
+function updatePanel(data) {
+    //
+}
 
